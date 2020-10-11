8bd8d37d133e9e9b295ba7620833353f84d9b407
Rishabh <jainr214@gmail.com>
2020-10-11 18:47:45 +0530

    added stemming

--- main.py
+++ main.py
@@ -8,22 +8,27 @@ from  nltk.stem import SnowballStemmer
 
   
 train_df= pd.read_csv("/home/rishabh/NLP/hate speech/data.1/train_E6oV3lV.csv",index_col="id")
-test_df=pd.read_csv("/home/rishabh/NLP/hate speech/data.1/test_tweets_anuFYb8.csv",index_col="id")
+test_df=pd.read_csv("/home/rishabh/NLP/hate speech/data.1/test_tweets_anuFYb8.csv",index_col="id",)
 
-#train_df= train_df.iloc[1900:2000,:]
+train_df= train_df.iloc[1900:2000,:]
 TEXT_CLEANING_RE = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
 
 stop_words=stopwords.words("english")
-stemmer = SnowballStemmer("english")
+stemmer = SnowballStemmer("english",ignore_stopwords=True)
 
-def preprocess(text,):
+def preprocess(text,stem= True):
     # Remove link,user and special characters
     text = re.sub(TEXT_CLEANING_RE, ' ', str(text).lower()).strip()
     tokens = []
     for token in text.split():
         if token not in stop_words:
-            tokens.append(token)
+            if stem:
+                tokens.append(stemmer.stem(token))
+            else:
+                tokens.append(token)
+
     return " ".join(tokens)
 
+#print(preprocess("love loving ,tvx lovely"))
+train_df['tweet1'] = train_df['tweet'].apply(lambda text: preprocess(text))
 
-train_df['tweet1'] = train_df['tweet'].apply(lambda x: preprocess(x))
