90445bbbd6b2201ffe023a24cded0c260460ec78
Rishabh <jainr214@gmail.com>
2020-10-13 00:59:40 +0530

    added a diff method to train word2vec model

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import pandas as pd
 
 import nltk,re
 from gensim.models import Word2Vec
-
+#import gensim
 from nltk.corpus import stopwords
 from  nltk.stem import SnowballStemmer
 
 #nltk.download('stopwords')
 
-  
+#W2V_SIZE = 300
+#W2V_WINDOW = 7
+#W2V_EPOCH = 32
+#W2V_MIN_COUNT = 10
+
 train_df= pd.read_csv("/home/rishabh/NLP/hate speech/data.1/train_E6oV3lV.csv",index_col="id")
 test_df=pd.read_csv("/home/rishabh/NLP/hate speech/data.1/test_tweets_anuFYb8.csv",index_col="id",)
 
@@ -37,6 +41,17 @@ train_df['tweet1'] = train_df['tweet'].apply(lambda text: preprocess(text))
 
 
 documents = [t1.split() for t1 in train_df.tweet1] 
+
+#w2v_model = gensim.models.word2vec.Word2Vec(size=W2V_SIZE,window=W2V_WINDOW,min_count=W2V_MIN_COUNT, workers=8)
+#w2v_model.build_vocab(documents)
+#
+#
+#words2 = w2v_model.wv.vocab.keys()
+#vocab_size = len(words2)
+#print("Vocab size", vocab_size)
+#
+#w2v_model.train(documents, total_examples=len(documents), epochs=W2V_EPOCH)
+#w2v_model.most_similar("hate")
 
 model = Word2Vec(documents)
 
